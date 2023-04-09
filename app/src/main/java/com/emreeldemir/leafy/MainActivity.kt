345ac3c765be0e5433e5ca1929dc8f344a234b6d
Emre Eldemir <emreeldemir893@gmail.com>
2023-04-09 17:40:52 +0300

    view binding enabled in main activity

--- app/src/main/java/com/emreeldemir/leafy/MainActivity.kt
+++ app/src/main/java/com/emreeldemir/leafy/MainActivity.kt
@@ -2,10 +2,16 @@ package com.emreeldemir.leafy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emreeldemir.leafy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // View Binding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
