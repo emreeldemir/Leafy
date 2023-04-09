53e081798eeafab024c18a576c71833b60165c15
Emre Eldemir <emreeldemir893@gmail.com>
2023-04-09 17:00:27 +0300

    splash activity modified

--- app/src/main/java/com/emreeldemir/leafy/SplashActivity.kt
+++ app/src/main/java/com/emreeldemir/leafy/SplashActivity.kt
@@ -1,11 +1,19 @@
 package com.emreeldemir.leafy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+
+        }, 3000)
+
     }
 }
