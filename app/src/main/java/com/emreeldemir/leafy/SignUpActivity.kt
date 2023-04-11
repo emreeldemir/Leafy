3fc5dfc5023297107f7cc2ec526d81cce8d57cfd
Emre Eldemir <emreeldemir893@gmail.com>
2023-04-11 15:03:58 +0300

    firebase auth and view binding enabled in SignUpActivity.kt

--- app/src/main/java/com/emreeldemir/leafy/SignUpActivity.kt
+++ app/src/main/java/com/emreeldemir/leafy/SignUpActivity.kt
@@ -2,10 +2,21 @@ package com.emreeldemir.leafy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emreeldemir.leafy.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+
+    // View Binding
+    private lateinit var binding: ActivitySignUpBinding
+
+    // Firebase Authentication
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
