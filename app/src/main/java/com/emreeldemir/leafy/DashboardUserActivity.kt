e4129ed2c0441a40707e2e812066bd3e06d617b5
Emre Eldemir <emreeldemir893@gmail.com>
2023-04-11 19:52:56 +0300

    view binding, firebase auth and checkUser method implemented in DashboardUserActivity.kt

--- app/src/main/java/com/emreeldemir/leafy/DashboardUserActivity.kt
+++ app/src/main/java/com/emreeldemir/leafy/DashboardUserActivity.kt
@@ -1,11 +1,56 @@
 package com.emreeldemir.leafy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emreeldemir.leafy.databinding.ActivityDashboardUserBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class DashboardUserActivity : AppCompatActivity() {
+
+    // View Binding
+    private lateinit var binding: ActivityDashboardUserBinding
+
+    // Firebase Auth
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard_user)
+        binding = ActivityDashboardUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Init Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        checkUser()
+
+        // Handle Click, Logout
+        binding.logoutButton.setOnClickListener {
+            firebaseAuth.signOut()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+    }
+
+    private fun checkUser() {
+// Get Current User
+        val firebaseUser = firebaseAuth.currentUser
+
+        if (firebaseUser == null) {
+            /**
+             * User not logged in,
+             * The user can stay in the DashboardUserActivity
+             * without login
+             */
+            binding.subtitleTextView.text = "You are not logged in"
+
+        }
+
+        else {
+            // User is logged in, get and show user info
+            val email = firebaseUser.email
+            // Set textView in Dashboard
+            binding.subtitleTextView.text = email
+        }
     }
 }
