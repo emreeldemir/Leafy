22f5432001b5bb885fac7363c67e10f05ffac37b
Emre Eldemir <emreeldemir893@gmail.com>
2023-04-11 19:17:47 +0300

    view binding, firebase auth and progress dialog implemented in LoginActivity.kt

--- app/src/main/java/com/emreeldemir/leafy/LoginActivity.kt
+++ app/src/main/java/com/emreeldemir/leafy/LoginActivity.kt
@@ -1,11 +1,36 @@
 package com.emreeldemir.leafy
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emreeldemir.leafy.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    // View Binding
+    private lateinit var binding: ActivityLoginBinding
+
+    // Firebase Authentication
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    // Progress Dialog
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Init Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Init Progress Dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+
+
     }
 }
