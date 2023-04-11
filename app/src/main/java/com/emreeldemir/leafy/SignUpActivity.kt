d7614da5fa9badc5ae0adcb80cb03b63a1015cde
Emre Eldemir <emreeldemir893@gmail.com>
2023-04-11 15:20:14 +0300

    validate data method implemented

--- app/src/main/java/com/emreeldemir/leafy/SignUpActivity.kt
+++ app/src/main/java/com/emreeldemir/leafy/SignUpActivity.kt
@@ -3,6 +3,8 @@ package com.emreeldemir.leafy
 import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.emreeldemir.leafy.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -16,6 +18,10 @@ class SignUpActivity : AppCompatActivity() {
 
     // Progress Dialog
     private lateinit var progressDialog: ProgressDialog
+
+    private var name = ""
+    private var email = ""
+    private var password = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
@@ -29,6 +35,57 @@ class SignUpActivity : AppCompatActivity() {
         progressDialog.setTitle("Please wait")
         progressDialog.setCanceledOnTouchOutside(false)
 
+    // Handle Click, Start Register User
+
+        binding.signUpButton.setOnClickListener {
+            validateData()
+        }
+
+
 
     }
+
+    private fun validateData() {
+        // Get (Input) Data
+        name = binding.editTextName.text.toString().trim()
+        email = binding.editTextEmail.text.toString().trim()
+        password = binding.editTextPassword.text.toString().trim()
+        val cPassword = binding.editTextCPassword.text.toString().trim()
+
+        // Validate Data
+        if (name.isEmpty()) {
+            Toast.makeText(this, "Please enter name", Toast.LENGTH_SHORT).show()
+        }
+
+        else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Invalid email pattern", Toast.LENGTH_SHORT).show()
+        }
+
+        else if (password.isEmpty()) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show()
+        }
+
+        else if (cPassword.isEmpty()) {
+            Toast.makeText(this, "Please confirm password", Toast.LENGTH_SHORT).show()
+        }
+
+        else if (password != cPassword) {
+            Toast.makeText(this, "Password doesn't match", Toast.LENGTH_SHORT).show()
+        }
+
+        else {
+            createUserAccount()
+        }
+
+
+
+
+
+    }
+
+    private fun createUserAccount() {
+
+    }
+
+
 }
