ef0acd0f8ecb22e0b73bf9dcd332d43469e77342
Emre Eldemir <emreeldemir893@gmail.com>
2023-04-11 16:02:29 +0300

    createUserAccount and updateUserInfo methods completed in SignUpActivity.kt

--- app/src/main/java/com/emreeldemir/leafy/SignUpActivity.kt
+++ app/src/main/java/com/emreeldemir/leafy/SignUpActivity.kt
@@ -1,12 +1,14 @@
 package com.emreeldemir.leafy
 
 import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
 import com.emreeldemir.leafy.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -27,20 +29,19 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-    // Init Firebase Auth
+        // Init Firebase Auth
         firebaseAuth = FirebaseAuth.getInstance()
 
-    // Init Progress Dialog
+        // Init Progress Dialog
         progressDialog = ProgressDialog(this)
         progressDialog.setTitle("Please wait")
         progressDialog.setCanceledOnTouchOutside(false)
 
-    // Handle Click, Start Register User
+        // Handle Click, Start Register User
 
         binding.signUpButton.setOnClickListener {
             validateData()
         }
-
 
 
     }
@@ -55,35 +56,80 @@ class SignUpActivity : AppCompatActivity() {
         // Validate Data
         if (name.isEmpty()) {
             Toast.makeText(this, "Please enter name", Toast.LENGTH_SHORT).show()
-        }
-
-        else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             Toast.makeText(this, "Invalid email pattern", Toast.LENGTH_SHORT).show()
-        }
-
-        else if (password.isEmpty()) {
+        } else if (password.isEmpty()) {
             Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show()
-        }
-
-        else if (cPassword.isEmpty()) {
+        } else if (cPassword.isEmpty()) {
             Toast.makeText(this, "Please confirm password", Toast.LENGTH_SHORT).show()
-        }
-
-        else if (password != cPassword) {
+        } else if (password != cPassword) {
             Toast.makeText(this, "Password doesn't match", Toast.LENGTH_SHORT).show()
-        }
-
-        else {
+        } else {
             createUserAccount()
         }
-
-
-
 
 
     }
 
     private fun createUserAccount() {
+        // Show Progress Dialog
+        progressDialog.setMessage("Creating Account...")
+        progressDialog.show()
+
+        // Create user in Firebase Auth
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                // Account created successfully
+                progressDialog.dismiss()
+                Toast.makeText(this, "Account Created Successfully", Toast.LENGTH_SHORT).show()
+
+                // Add user info to Firebase Realtime Database
+                updateUserInfo()
+            }
+            .addOnFailureListener { e ->
+                // Failed to create account
+                progressDialog.dismiss()
+                Toast.makeText(this, "Failed to create account: ${e.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+    }
+
+    private fun updateUserInfo() {
+        progressDialog.setMessage("Saving User Info...")
+
+        // Timestamp
+        val timestamp = System.currentTimeMillis()
+
+        // Get uid of current user
+        val uid = firebaseAuth.uid
+
+        // Setup data to save
+        val hashMap = HashMap<String, Any?>()
+        hashMap["uid"] = uid
+        hashMap["email"] = email
+        hashMap["name"] = name
+        hashMap["profileImage"] = ""
+        hashMap["userType"] = "user"
+        hashMap["timestamp"] = timestamp
+
+        // Save to Firebase Realtime Database
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(uid!!).setValue(hashMap)
+            .addOnSuccessListener {
+                // User info saved, open user dashboard
+                progressDialog.dismiss()
+                Toast.makeText(this, "Account created...", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this@SignUpActivity, DashboardUserActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener { e ->
+                // Failed to save user info
+                progressDialog.dismiss()
+                Toast.makeText(this, "Failed to save user info: ${e.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
 
     }
 
