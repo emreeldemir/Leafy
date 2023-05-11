d08b533583c7f76e4fbd1d61f1c2651c9a06b72f
Emre Eldemir <emreeldemir893@gmail.com>
2023-05-11 19:47:08 +0300

    ModelCategory and AdapterCategory initialized

---
+++ app/src/main/java/com/emreeldemir/leafy/AdapterCategory.kt
@@ -0,0 +1,4 @@
+package com.emreeldemir.leafy
+
+class AdapterCategory {
+}
---
+++ app/src/main/java/com/emreeldemir/leafy/ModelCategory.kt
@@ -0,0 +1,34 @@
+package com.emreeldemir.leafy
+
+class ModelCategory {
+
+    /**
+     * Variables should be match with the variables in the Firebase
+     */
+
+    var id: String = ""
+    var category: String = ""
+    var timestamp: Long = 0
+    var uid: String = ""
+
+    /**
+     * Empty constructor is needed for Firebase
+     */
+    constructor() {}
+
+    /**
+     * Constructor with parameters
+     */
+    constructor(id: String, category: String, timestamp: Long, uid: String) {
+        this.id = id
+        this.category = category
+        this.timestamp = timestamp
+        this.uid = uid
+    }
+
+
+
+
+
+
+}
