1499e3fde9533486bc491b6895fa45282ae0f5e3
Emre Eldemir <emreeldemir893@gmail.com>
2023-05-23 06:31:11 +0300

    pdf add activity init

---
+++ app/src/main/java/com/emreeldemir/leafy/PdfAddActivity.kt
@@ -0,0 +1,11 @@
+package com.emreeldemir.leafy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PdfAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pdf_add)
+    }
+}
